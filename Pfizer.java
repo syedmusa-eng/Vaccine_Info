21293bb8b6d839fb5a1e1bd76aef979516718b3e
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-21 23:52:36 +0200

    extends abstract class RNA

---
+++ Pfizer.java
@@ -0,0 +1,17 @@
+package it.polito.oop.vaccines;
+
+public class Pfizer extends RNA{
+
+	public Pfizer() {
+		super("modRNA in lipid nanoparticles", .95, 12, 2, 25);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public int getIntervalBetweenDoses() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
